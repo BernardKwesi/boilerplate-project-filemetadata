8b61602e81d53030932b1254d466ec5289859619
BernardKwesi <bernarddormon31@gmail.com>
2022-01-21 20:16:05 +0000

    Challenge completed

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var bodyparser = require("body-parser");
+var multer = require('multer');
 var app = express();
+
+const fileStorageEngine = multer.diskStorage({ destination: (req,file,cb)=>{
+  cb(null, "public/uploads");
+},
+filename :(req,file,cb)=>{
+  cb(null,file.originalname );
+} });
+
+const upload = multer({storage: fileStorageEngine});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+// Body-parser middleware
+app.use(bodyparser.urlencoded({extended:false}))
+
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single('upfile'),(req,res)=>{
+
+console.log(req.file);
+
+      res.json({
+        name:req.file.originalname,
+        type:req.file.mimetype,
+        size:req.file.size
+      });
+
+})
 
 
 
